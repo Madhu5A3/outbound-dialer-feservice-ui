025b9ecc905a6d558583c21f130c444afe39a509
MadhuMudunuri <MMudunuri@serenova.com>
2019-10-21 20:36:05 +0530

    outbound

---
+++ src/main/java/com/serenova/outbound/dialer/feservice/FeServicesApplication.java
@@ -0,0 +1,13 @@
+package com.serenova.outbound.dialer.feservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serenova/outbound/dialer/feservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serenova.outbound.dialer.feservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
